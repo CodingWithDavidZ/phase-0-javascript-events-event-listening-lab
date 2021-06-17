ebe35591666d5af9dbf24d0bca9b563c71585ed6
David Zombar <davidzombar@gmail.com>
2021-06-17 09:49:55 -0400

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    function clickAlert() { alert("I Was Clicked!")}
+    input.addEventListener('click', clickAlert)
 }
